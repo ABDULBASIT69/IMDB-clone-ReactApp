e2605c6efd076f88471e33e6351fbb37ae78d5ca
ABDULBASIT69 <chudharyabdulbasit@gmail.com>
2023-01-23 10:46:04 +0500

    IMDB clone React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Home from './pages/home/Home'
+import Movie from './pages/movieDetail/Movie'
+import MovieList from './components/movielist/MovieList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='movie/:id' element={ <Movie /> } />
+          <Route path='movies/:type' element={<MovieList />} />
+          <Route path='/*' element={<h1>Error Page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
